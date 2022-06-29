e45e769999932ace6bdec184b8e35bed96f1fb01
Miguel Jones <mjcoqui@gmail.com>
2022-06-29 03:28:22 +0000

    Day 2 edit with get request and user input for pokemon

--- poke.py
+++ poke.py
@@ -1,6 +1,31 @@
 import requests
 
-url = 'https://pokeapi.co/api/v2/pokemon/charmander/'
+validInput = False
+while not validInput:
+  starter = input("Please enter the pokemon that you choose: ")
+  url = 'https://pokeapi.co/api/v2/pokemon/' + starter.lower()
+  response = requests.get(url)
 
-response = requests.get(url)
-print(response.json())
+  try:
+    pokemonData = response.json()
+    validInput = True
+  except:
+    print("That is not a pokemon that exists in the Nine regions")
+    print('')
+
+
+print('The pokemon you chose is ' + pokemonData['name'])
+count = 0
+types = ''
+for el in pokemonData['types']:
+  count += 1
+  if count > 1:
+    types += ' and ' 
+  types += str(el['type']['name'])
+  
+
+if count == 1:
+  print(f'The pokemon\'s type is {types}')
+else:
+  print(f'The pokemon\'s types are {types}')
+
